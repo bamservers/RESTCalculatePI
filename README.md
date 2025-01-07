fb9925fc22cfafaa5266a586b0f3991df3eba5a3
Xoph <al475@hotmail.com>
2025-01-07 18:54:50 -0500

    Now using the Bailey-Borwein-Plouffe algorithm to generate the Nth fractional digit in hex. Also updated the readme with build & run instructions for the project. Also removed the directory mounting code, because it was just for testing. Added more instructions to the readme file

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # RESTCalculatePI
-A simple REST service used to calculate PI given the # of decimals required (current maximum is 20)
+A simple REST service used to calculate PI given the # of fractional digits required
+
+# Setup
+- Download/Install DART
+- Clone Repo: https://github.com/bamservers/RESTCalculatePI.git
+
+# To Run
+```
+dart run RESTCalculatePI.dart
+```
+
+# To Build (Windows, Optional)
+```
+dart compile exe RESTCalculatePI.dart
+```
+
+# Usage
+By default, the project runs on port 8080 (configurable in the code). When running, the following URLs will be available:
+
+http://127.0.0.1:8080/HelloWorld
+- Confirms the web server is up
+
+http://127.0.0.1:8080/pi/5
+- Calculates PI from the 5th fractional digit onwards. 
+
--- RESTCalculatePI.dart
+++ RESTCalculatePI.dart
@@ -10,11 +10,11 @@ import 'package:shelf_router/shelf_router.dart';
 void main() async {
   final app = Router();
 
-  // Define a GET endpoint to calculate PI to the nth decimal place
+  // Define a GET endpoint to calculate PI to the nth hex place
   app.get('/pi/<n>', (shelf.Request request, String n) {
     print ("New request from: " + request.url.path);
-    final int decimalPlaces = int.tryParse(n) ?? 0;
-    final piValue = calculatePi(decimalPlaces);
+    final int hexPlaces = int.tryParse(n) ?? 0;
+    final piValue = calculatePi(hexPlaces);
     return shelf.Response.ok(piValue.toString());
   });
 
@@ -34,13 +34,82 @@ void main() async {
   });  // Start the server
 
   // Mount a directory, and show the directory if there's no default index.html file
-  app.mount('/RESTCalculatePI_Files/', stat.createStaticHandler (r'.\', defaultDocument: 'index.html', listDirectories: true, serveFilesOutsidePath: false));
+  // was added for testing purposes, leaving it commented out for now.
+  // app.mount('/RESTCalculatePI_Files/', stat.createStaticHandler (r'.\', defaultDocument: 'index.html', listDirectories: true, serveFilesOutsidePath: false));
 
   final server = await io.serve(app, '127.0.0.1', 8080);// using localhost means 127.0.0.1 won't work. Stick to 127.0.0.1
   print('Server listening on ${server.address}:${server.port}');
 }
 
-// Function to calculate PI to the nth decimal place (up to 20, limitation in DART). Later, we can manually generate further decimals for PI directly.
-double calculatePi(int n) {
-  return double.parse(pi.toStringAsFixed(n));
+// Function to calculate PI to the nth hexadecimal place. In DART, the build-in constant holding PI can provide only up to 20 fractional decimal digits.
+String calculatePi(int n) {
+  String out = "";
+  // Provide the decimal values of PI in a range of n to n+10
+  
+  for (int cnt = n;cnt < n + 14;cnt++)
+  {
+    out += piDigit (cnt).toRadixString(16);
+  }
+
+  return out;//double.parse(pi.toStringAsFixed(n));
+}
+
+
+// Bailey-Borwein-Plouffe-Algorithm from https://web.archive.org/web/20150627225748/http://en.literateprograms.org/Pi_with_the_BBP_formula_%28Python%29 & https://stackoverflow.com/q/12449430
+/**
+ * Computes the nth digit of Pi in base-16.
+ *
+ * If n < 0, return -1.
+ *
+ * @param n The digit of Pi to retrieve in base-16.
+ * @return The nth digit of Pi in base-16.
+ */
+int piDigit(int n) {
+    if (n < 0) return -1;
+
+    n -= 1;
+    double x = 4 * piTerm(1, n) - 2 * piTerm(4, n) -
+               piTerm(5, n) - piTerm(6, n);
+    x = x - x.floorToDouble();//Math.floor(x);
+
+    return /*(int)*/(x * 16).toInt();
+}
+
+double piTerm(int j, int n) {
+    // Calculate the left sum
+    double s = 0;
+    for (int k = 0; k <= n; ++k) {
+        int r = 8 * k + j;
+        s += powerMod(16, n-k, r) / /*(double)*/ r.toDouble();
+        s = s - /*Math.floor*/(s.floorToDouble());
+    }
+
+    // Calculate the right sum
+    double t = 0;
+    int k = n+1;
+    // Keep iterating until t converges (stops changing)
+    while (true) {
+        int r = 8 * k + j;
+        double newt = t + /*Math.*/pow(16, n-k) / r;
+        if (t == newt) {
+            break;
+        } else {
+            t = newt;
+        }
+        ++k;
+    }
+
+    return s+t;
+}
+
+int powerMod(int base, int exponent, int modulus)
+{
+    /*BigInteger*/int n = 1;//BigInteger.ONE;
+    for (int i = 0; i < exponent; ++i) {
+        n = n/*.multiply*/ * (/*BigInteger.valueOf*/(base));
+    }
+    // n = n.mod(BigInteger.valueOf(modulus));
+    n = n % modulus;
+    // n is now guaranteed to be < modulus, so n definitely fits into an int
+    return n.toInt();//n.intValue();
 }
